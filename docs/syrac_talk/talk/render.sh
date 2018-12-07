b67a98d301f8e21499a487aa217c294baf7f153f
mklau <matthewklau@fas.harvard.edu>
2018-12-07 18:28:59 -0500

    adds goals and capsule list

--- docs/syrac_talk/talk/render.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-
-
-# http://rmarkdown.rstudio.com/beamer_presentation_format.html
-
-# pandoc -s -S -t revealjs --mathjax -o test.html 
-pandoc -s -t beamer -o MKLau_datapass_2018.ppt talk.md
